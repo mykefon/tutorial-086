eebffec23ef43520ab5d14cc10e163a93a255fd7
d@t@secure@100 <mykefon@gmail.com>
2022-07-19 00:45:56 -0400

    hhmm

--- build/git_update.sh
+++ build/git_update.sh
@@ -30,13 +30,13 @@ VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-  VNUM1=v$((VNUM1+1))
+  VNUM1=v$((VNUM1+1+1))
 elif [[ $VERSION == 'minor' ]]
 then
-  VNUM2=$((VNUM2+1))
+  VNUM2=$((VNUM2+1+1))
 elif [[ $VERSION == 'patch' ]]
 then
-  VNUM3=$((VNUM3+1))
+  VNUM3=$((VNUM3+1+1))
 else
   echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
   exit 1
